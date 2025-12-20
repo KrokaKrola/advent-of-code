f085ea59b744ecc4b7ba9eab662215e986ea799a
KrokaKrola <harlamsan@gmail.com>
2025-12-20 23:38:03 +0200

    2025 day 7 part 1 refined solution

--- 2025/7/solution.py
+++ 2025/7/solution.py
@@ -1,7 +1,7 @@
 import os
 
 script_dir = os.path.dirname(__file__)
-file_name = "input.txt"
+file_name = "input-test.txt"
 absolute_file_path = os.path.join(script_dir, file_name)
 
 with open(absolute_file_path) as f:
@@ -29,4 +29,36 @@ def solution_1():
     print(total_beams)
 
 
-solution_1()
+def solution_1_1():
+    active_columns = {data[0].index("S")}
+
+    total_beams = 0
+    height = len(data)
+    width = len(data[0])
+
+    for r in range(1, height):
+        next_active_columns = set()
+
+        for c in active_columns:
+            char = data[r][c]
+
+            if char == "^":
+                total_beams += 1
+
+                if c - 1 >= 0:
+                    next_active_columns.add(c - 1)
+
+                if c + 1 < width:
+                    next_active_columns.add(c + 1)
+            else:
+                next_active_columns.add(c)
+
+        active_columns = next_active_columns
+
+        if not active_columns:
+            break
+
+    print(total_beams)
+
+
+solution_1_1()
